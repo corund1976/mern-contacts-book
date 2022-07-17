128ca994c88add1864e2ceafb97d656e210130a5
corund1976 <corund1976@gmail.com>
2022-07-17 23:41:55 +0300

    mongo

--- server/bin/server.js
+++ server/bin/server.js
@@ -4,9 +4,9 @@ import app from '../app.js'
 
 mongoose.Promise = global.Promise
 
-const { USER, PASSWORD, HOST, DATABASE, PORT = 3000 } = process.env
+const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_DATABASE, PORT = 3000 } = process.env
 
-const uri = `mongodb+srv://${USER}:${PASSWORD}@${HOST}/${DATABASE}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`
 
 mongoose
   .connect(uri)
--- server/service/models/tokenSchema.js
+++ server/service/models/tokenSchema.js
@@ -2,16 +2,19 @@ import mongoose from 'mongoose'
 
 const { Schema, SchemaTypes, model } = mongoose
 
-const tokenSchema = new Schema({
-  userId: {
-    type: SchemaTypes.ObjectId,
-    ref: 'user',
+const tokenSchema = new Schema(
+  {
+    userId: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    }
   },
-  refreshToken: {
-    type: String,
-    required: true,
-  }
-})
+  { versionKey: false, timestamps: false },
+)
 
 const Token = model('token', tokenSchema)
 
